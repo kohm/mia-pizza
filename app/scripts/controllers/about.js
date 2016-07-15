87ea430a5c6cc6016b2ec24f682074fe8065c5f5
Lucio Poveda <lucio.poveda@gmail.com>
2016-07-14 22:51:56 -0300

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name miaPizzaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the miaPizzaApp
+ */
+angular.module('miaPizzaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name miaPizzaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the miaPizzaApp
+ */
+angular.module('miaPizzaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
